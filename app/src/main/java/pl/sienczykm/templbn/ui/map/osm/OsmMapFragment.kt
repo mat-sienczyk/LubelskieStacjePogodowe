ba26a62f8a9eda1226bfaa8e41b6d09a0d8cbcbe
mat-sienczyk <mati15190@gmail.com>
2020-09-08 18:11:25 +0200

    apply filters to osm map

--- app/src/main/java/pl/sienczykm/templbn/ui/map/osm/OsmMapFragment.kt
+++ app/src/main/java/pl/sienczykm/templbn/ui/map/osm/OsmMapFragment.kt
@@ -14,12 +14,11 @@ import pl.sienczykm.templbn.databinding.FragmentOsmMapBinding
 import pl.sienczykm.templbn.db.model.AirStationModel
 import pl.sienczykm.templbn.db.model.BaseStationModel
 import pl.sienczykm.templbn.db.model.WeatherStationModel
+import pl.sienczykm.templbn.ui.map.AirFilter
 import pl.sienczykm.templbn.ui.map.BaseMapFragment
 import pl.sienczykm.templbn.ui.map.MapNavigator
-import pl.sienczykm.templbn.utils.getDrawableWithColor
-import pl.sienczykm.templbn.utils.isLocationPermissionGranted
-import pl.sienczykm.templbn.utils.isNightModeActive
-import pl.sienczykm.templbn.utils.show
+import pl.sienczykm.templbn.ui.map.WeatherFilter
+import pl.sienczykm.templbn.utils.*
 
 
 class OsmMapFragment : BaseMapFragment<FragmentOsmMapBinding>(), MapNavigator {
@@ -32,6 +31,7 @@ class OsmMapFragment : BaseMapFragment<FragmentOsmMapBinding>(), MapNavigator {
 
     private var mapView: MapView? = null
     private var selectedStation: BaseStationModel? = null
+    private val markers = arrayListOf<Marker>()
 
     override fun getLayoutId() =
         R.layout.fragment_osm_map
@@ -76,6 +76,11 @@ class OsmMapFragment : BaseMapFragment<FragmentOsmMapBinding>(), MapNavigator {
 
     override fun updateMap() {
         mapView?.let { mapView ->
+            mapView.apply {
+                overlays.removeAll(markers)
+                markers.clear()
+                invalidate()
+            }
             viewModel.stations.value?.let { stations ->
                 val infoView = ClickableMarkerInfoWindow(
                     mapView,
@@ -84,14 +89,27 @@ class OsmMapFragment : BaseMapFragment<FragmentOsmMapBinding>(), MapNavigator {
                 )
 
                 stations.forEach { stationModel ->
-                    mapView.overlays.add(Marker(mapView).apply {
+
+                    val icon = when (stationModel) {
+                        is AirStationModel -> airLocationMarker()
+                        is WeatherStationModel -> weatherLocationMarker()
+                        else -> null
+                    }
+
+                    val textIcon = when (stationModel) {
+                        is AirStationModel -> airStringMarker(stationModel)
+                        is WeatherStationModel -> weatherStringMarker(stationModel)
+                        else -> null
+                    }
+
+                    if (icon == null && textIcon == null) return@forEach
+
+                    markers.add(Marker(mapView).apply {
                         position =
                             GeoPoint(stationModel.latitude, stationModel.longitude)
                         title = stationModel.getName()
-                        icon = requireContext().getDrawableWithColor(
-                            R.drawable.ic_map_marker,
-                            if (stationModel is AirStationModel) Color.BLUE else Color.GREEN
-                        )
+                        if (icon != null) setIcon(icon)
+                        if (textIcon != null) setTextIcon(textIcon)
                         setAnchor(
                             Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM
                         )
@@ -105,8 +123,79 @@ class OsmMapFragment : BaseMapFragment<FragmentOsmMapBinding>(), MapNavigator {
                         }
                     })
                 }
+                mapView.apply {
+                    overlays.addAll(markers)
+                    invalidate()
+                }
             }
         }
+    }
+
+    private fun weatherLocationMarker() =
+        when (viewModel.weatherFilter) {
+            WeatherFilter.LOCATION -> requireContext().getDrawableWithColor(
+                R.drawable.ic_map_marker, Color.GREEN)
+            else -> null
+        }
+
+    private fun airLocationMarker() =
+        when (viewModel.airFilter) {
+            AirFilter.LOCATION -> requireContext().getDrawableWithColor(
+                R.drawable.ic_map_marker, Color.BLUE)
+            else -> null
+        }
+
+    private fun weatherStringMarker(stationModel: WeatherStationModel) =
+        when (viewModel.weatherFilter) {
+            WeatherFilter.NOTHING -> null
+            WeatherFilter.TEMPERATURE -> stringMarker(stationModel,
+                stationModel.getParsedTemperature(1)
+                    ?.plus(getString(R.string.celsius_degree)))
+            WeatherFilter.WIND -> stringMarker(stationModel, stationModel.getParsedWind(1)
+                ?.plus(if (stationModel.convertWind) getString(R.string.km_per_hour) else getString(
+                    R.string.m_per_sec))
+            )
+            WeatherFilter.HUMIDITY -> stringMarker(stationModel, stationModel.getParsedHumidity(1)
+                ?.plus(getString(R.string.percent)))
+            WeatherFilter.RAIN_TODAY -> stringMarker(stationModel, stationModel.getParsedRain(1)
+                ?.plus(getString(R.string.milliliters)))
+            else -> null
+        }
+
+
+    private fun airStringMarker(stationModel: AirStationModel) = when (viewModel.airFilter) {
+        AirFilter.NOTHING -> null
+        AirFilter.PM10 -> stringMarker(stationModel,
+            stationModel.getValue(AirStationModel.AirSensorType.PM10)
+                ?.roundAndGetString()?.plus(getString(R.string.microgram_per_cubic_metre)))
+        AirFilter.PM25 -> stringMarker(stationModel,
+            stationModel.getValue(AirStationModel.AirSensorType.PM25)
+                ?.roundAndGetString()?.plus(getString(R.string.microgram_per_cubic_metre)))
+        AirFilter.O3 -> stringMarker(stationModel,
+            stationModel.getValue(AirStationModel.AirSensorType.O3)
+                ?.roundAndGetString()?.plus(getString(R.string.microgram_per_cubic_metre)))
+        AirFilter.NO2 -> stringMarker(stationModel,
+            stationModel.getValue(AirStationModel.AirSensorType.NO2)
+                ?.roundAndGetString()?.plus(getString(R.string.microgram_per_cubic_metre)))
+        AirFilter.SO2 -> stringMarker(stationModel,
+            stationModel.getValue(AirStationModel.AirSensorType.SO2)
+                ?.roundAndGetString()?.plus(getString(R.string.microgram_per_cubic_metre)))
+        AirFilter.C6H6 -> stringMarker(stationModel,
+            stationModel.getValue(AirStationModel.AirSensorType.C6H6)
+                ?.roundAndGetString()?.plus(getString(R.string.microgram_per_cubic_metre)))
+        AirFilter.CO -> stringMarker(stationModel,
+            stationModel.getValue(AirStationModel.AirSensorType.CO)
+                ?.roundAndGetString()?.plus(getString(R.string.milligram_per_cubic_metre)))
+        else -> null
+    }
+
+
+    private fun stringMarker(
+        stationModel: BaseStationModel,
+        value: String?,
+    ): String? {
+        if (stationModel.isDateObsoleteOrNull()) return null
+        return value
     }
 
     private fun onMarkerWindowTouch(): () -> Unit = {
